e549abda07d77da6f11f92eec8e3eba70259a475
Breno Carvalho <carv.breno.dev@gmail.com>
2024-06-12 20:56:20 -0300

    add: add first api route mocking

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,16 @@
+export async function GET() {
+	const response = await fetch("https://api.origamid.online/conta/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username: "dog",
+			password: "dog",
+		}),
+	});
+	if (!response.ok) {
+		return Response.json({ error: "Dados incorretos" }, { status: 401 });
+	}
+	return Response.json({ status: "ok" });
+}
